35f0a629f9150b490bad231a3f65be3f2d29ce64
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2015-08-09 02:44:40 -0400

    added tests

--- test/unit/lib/perf/EventDispatch/EventTest.php
+++ test/unit/lib/perf/EventDispatch/EventTest.php
@@ -19,4 +19,59 @@ class EventTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame($id, $event->getId());
     }
+
+    /**
+     *
+     */
+    public function testHasParameterWithNotExistingParameter()
+    {
+        $id = 'foo.bar';
+
+        $event = new Event($id);
+
+        $this->assertFalse($event->hasParameter('baz'));
+    }
+
+    /**
+     *
+     */
+    public function testHasParameterWithExistingParameter()
+    {
+        $id = 'foo.bar';
+        $parameters = array(
+            'baz' => 'qux',
+        );
+
+        $event = new Event($id, $parameters);
+
+        $this->assertTrue($event->hasParameter('baz'));
+    }
+
+    /**
+     *
+     * @expectedException \DomainException
+     */
+    public function testGetParameterWithNotExistingParameter()
+    {
+        $id = 'foo.bar';
+
+        $event = new Event($id);
+
+        $event->getParameter('baz');
+    }
+
+    /**
+     *
+     */
+    public function testGetParameterWithExistingParameter()
+    {
+        $id = 'foo.bar';
+        $parameters = array(
+            'baz' => 'qux',
+        );
+
+        $event = new Event($id, $parameters);
+
+        $this->assertSame('qux', $event->getParameter('baz'));
+    }
 }
